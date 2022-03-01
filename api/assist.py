488888521eab76bf47e30596c99a68d3266d65fd
manav-1 <manav81101@gmail.com>
2022-03-01 11:21:23 +0530

    updates wrt to production

--- api/assist.py
+++ api/assist.py
@@ -16,13 +16,22 @@ from sqlalchemy import or_
 
 from datetime import datetime
 from sqlalchemy.sql import func
-
+import  json
 import time
 
 basedir = os.path.realpath(".")
 # print("connecting...")
-pg_engine = create_engine(
-    "postgresql://postgres:root@localhost:5432/placements")
+# pg_engine = create_engine(
+#     "postgresql://postgres:root@localhost:5432/placements")
+# pg_conn = pg_engine.connect()
+
+with open("api/config.json") as config_file:
+    config = json.loads(config_file.read())
+
+DATABASE_BASE_URL = f"postgresql://{config['DATABASES']['PLACEMENTS_PROD']['user']}:{config['DATABASES']['PLACEMENTS_PROD']['password']}@{config['DATABASES']['PLACEMENTS_PROD']['host']}:{config['DATABASES']['PLACEMENTS_PROD']['port']}"
+DATABASE_URL = f'{DATABASE_BASE_URL}/{config["DATABASES"]["PLACEMENTS_PROD"]["database"]}'
+
+pg_engine = create_engine(DATABASE_URL)
 pg_conn = pg_engine.connect()
 
 # pg_engine_inside = create_engine(
@@ -37,13 +46,13 @@ def drop_tables():
 
 def insert_rows():
     pg_conn.execute(
-        """INSERT INTo global.stream(stream_name) VALUES 
+        """INSERT INTO global.stream(stream_name) VALUES 
         ('BSc. Hons Computer Science'),
-        ('BSc. Hons ABC'),
-        ('BSc. Hons DEF'),
-        ('BSc. Hons GHI'),
-        ('BSc. Hons JKL'),
-        ('BSc. Hons MNO')
+        ('BCOM Hons.'),
+        ('BSc. Hons Mathematics'),
+        ('BSc. Hons Physics'),
+        ('BSc. Hons Chemistry'),
+        ('Bachelors of  Management Studies')
         """)
 
 
